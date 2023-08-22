1dcf2ef830abdbaeba468ce46baa32d646422083
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-08-21 23:59:15 -0400

    Enable cors

--- backend/api.js
+++ backend/api.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from "cors";
 import { database } from './database.js';
 
 const app = express();
 const port = 5000;
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
