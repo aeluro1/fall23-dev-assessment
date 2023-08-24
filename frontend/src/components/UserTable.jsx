6de5e7666bea144872a7f675f0e565f3f0f8fbc2
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-08-23 21:46:26 -0400

    Fixed table styling width

--- frontend/src/components/UserTable.jsx
+++ frontend/src/components/UserTable.jsx
@@ -6,12 +6,14 @@ import { Link } from "react-router-dom";
 import { UsersContext } from "../contexts/UsersContext";
 
 const useStyles = createStyles((theme) => ({
+  container: {
+    overflowX: "auto",
+    width: "min(80vw, 1000px)"
+  },
   table: {
-    display: "block",
-    width: "min(80vw, 1000px)",
+    minWidth: "100%",
     margin: "0 auto",
     borderCollapse: "collapse",
-    overflowX: "auto",
     "tr": {
       borderBottom: `1px solid ${theme.colors.gray[3]}`,
       textAlign: "center"
@@ -74,55 +76,57 @@ export default function UserTable() {
 
   return (
     <Paper shadow="md" p="sm">
-      <table className={classes.table}>
-        <thead>
-          <tr>
-            <th>Volunteer</th>
-            <th>Picture</th>
-            <th>Phone</th>
-            <th>Email</th>
-            <th>Rating</th>
-            <th>Status</th>
-            <th>Project</th>
-            <th>Modify</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.name}>
-              <td>{user.name}</td>
-              <td>
-                <img
-                    src={user.avatar}
-                    alt={user.name} 
-                    className={classes.pic}
-                />
-              </td>
-              <td>{user.phone}</td>
-              <td>{user.email}</td>
-              <td>{user.rating}</td>
-              {user.status ? (
-                <td style={{ color: "green" }}>Active</td>
-              ) : (
-                <td style={{ color: "red" }}>Inactive</td>
-              )}
-              <td>{user.hero_project}</td>
-              <td>
-                <div className={classes.mods}>
-                  <button className={classes.modBtn}>
-                    <Link to={`/edit/${user.id}`} style={{ color: "inherit" }}>
-                      <FontAwesomeIcon icon={faEdit} />
-                    </Link>
-                  </button>
-                  <button onClick={() => delUserHandler(user.id)} className={classes.modBtn}>
-                    <FontAwesomeIcon icon={faTrash} />
-                  </button>
-                </div>
-              </td>
+      <div className={classes.container}>
+        <table className={classes.table}>
+          <thead>
+            <tr>
+              <th>Volunteer</th>
+              <th>Picture</th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th>Rating</th>
+              <th>Status</th>
+              <th>Project</th>
+              <th>Modify</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.name}>
+                <td>{user.name}</td>
+                <td>
+                  <img
+                      src={user.avatar}
+                      alt={user.name}
+                      className={classes.pic}
+                  />
+                </td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
+                <td>{user.rating}</td>
+                {user.status ? (
+                  <td style={{ color: "green" }}>Active</td>
+                ) : (
+                  <td style={{ color: "red" }}>Inactive</td>
+                )}
+                <td>{user.hero_project}</td>
+                <td>
+                  <div className={classes.mods}>
+                    <button className={classes.modBtn}>
+                      <Link to={`/edit/${user.id}`} style={{ color: "inherit" }}>
+                        <FontAwesomeIcon icon={faEdit} />
+                      </Link>
+                    </button>
+                    <button onClick={() => delUserHandler(user.id)} className={classes.modBtn}>
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Paper>
   );
 }
