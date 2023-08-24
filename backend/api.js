02308a3e7fc37a46b4c2021d4bbe43318947d2af
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-08-24 01:40:15 -0400

    Fixed status code setting in get handlers

--- backend/api.js
+++ backend/api.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from "cors";
 import { database } from './database.js';
+import fs from "fs";
 
 const app = express();
 const port = 5000;
@@ -12,13 +13,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/bog/users', (req, res) => {
-    res.json(database).status(200);
+    res.status(200).json(database);
 });
 
-
 app.get('/api/bog/users/:id', (req, res) => {
-    const user = database.filter((user) => user.id === req.params.id)[0]
-    res.json(user).status(200)
+    const user = database.filter((user) => user.id === req.params.id)[0];
+    res.status(200).json(user);
 });
 
 
