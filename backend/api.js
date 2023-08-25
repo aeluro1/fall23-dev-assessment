689544a339e20cb708896af0efff546b62681a26
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-08-25 13:16:03 -0400

    Implemented post/put/delete

--- backend/api.js
+++ backend/api.js
@@ -21,6 +21,72 @@ app.get('/api/bog/users/:id', (req, res) => {
     res.status(200).json(user);
 });
 
+// For post/put/delete requests, just overwrite database.js
+function saveDB() {
+  const db = "export const database = " + JSON.stringify(database, null, 4) + ";";
+  fs.writeFile("./database.js", db, (err) => {
+    if (err) {
+      console.error("DB update failed");
+      return 1;
+    } else {
+      console.log("DB update succeeded");
+      return 0;
+    }
+  });
+}
+
+app.post("/api/bog/users", (req, res) => {
+  if (database.some((user) => user.id === req.body.id)) {
+    res.status(400).json(database.filter((user) => user.id === req.body.id)[0]);
+  } else {
+    database.push(req.body);
+    if (saveDB()) {
+      res.status(400);
+    } else {
+      res.status(200);
+    }
+    res.json(req.body);
+  }
+});
+
+app.put("/api/bog/users/:id", (req, res) => {
+  const newUser = {
+    ...req.body,
+    id: req.params.id
+  }
+  if (database.every((user) => user.id !== req.params.id)) {
+    res.status(400).json(newUser);
+  } else {
+    database.forEach((user, idx, arr) => {
+      if (user.id === req.params.id) {
+        arr[idx] = newUser;
+      }
+    });
+    if (saveDB()) {
+      res.status(400);
+    } else {
+      res.status(200);
+    }
+    res.json(newUser);
+  }
+});
+
+app.delete("/api/bog/users/:id", (req, res) => {
+  if (database.every((user) => user.id !== req.params.id)) {
+    res.status(400).json(req.body);
+  } else {
+    const idx = database.findIndex((user) => user.id === req.params.id);
+    const ret = database[idx];
+    database.splice(idx, 1);
+    if (saveDB()) {
+      res.status(400);
+    } else {
+      res.status(200);
+    }
+    res.json(ret);
+  }
+});
+
 
 // Start the server
 app.listen(port, () => {
